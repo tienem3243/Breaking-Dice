1b0b3d72abc15affa9aae0ecb3c56ed2c07552ea
trong7a2002 <tienem123.com@gmail.com>
2022-06-09 17:39:17 +0700

    -fix

--- Assets/Script/CardDisplay.cs
+++ Assets/Script/CardDisplay.cs
@@ -12,27 +12,29 @@ public class CardDisplay : MonoBehaviour
    
     private void Start()
     {
+        //set up sprite
         var character = Char.GetComponent<Class>();
         Sprite[] costList = Resources.LoadAll<Sprite>("Sprites/Dice/DiceStyle1");
         Sprite[] itemSlotList = Resources.LoadAll<Sprite>("Sprites/BaseCard/ItemSlot");
         Sprite charClass = Resources.Load<Sprite>("Sprites/Class/"+character.Classname);
+        //set up stat
         cardName.text = character.Classname;
         atk.text = character.Dame.ToString();
         hp.text = character.Hp.ToString();
         ability1.text = character.AbilityList[0].Name;
         ability2.text = character.AbilityList[1].Name;
-        Debug.Log(character.Items.Length);
+       //set item slot
         if (character.Items.Length != 0)
         {
             itemSlotCount.sprite = itemSlotList[character.Items.Length];
         }
+        //set cost
         if (character.AbilityList.Length != 0)
         {
             diceCost1.sprite = costList[character.AbilityList[0].DiceCost ];
             diceCost2.sprite = costList[character.AbilityList[1].DiceCost ];
         }
         IconOfClass.sprite = charClass;
-       
         Instantiate(character.gameObject, charStand);
     }
 
