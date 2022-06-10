16823df9d3ca0aea7ca8ed0ddc051e47ea925078
trong7a2002 <tienem123.com@gmail.com>
2022-06-10 10:04:14 +0700

    -fix

--- Assets/Script/CardDisplay.cs
+++ Assets/Script/CardDisplay.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class CardDisplay : MonoBehaviour
 {
@@ -9,21 +10,28 @@ public class CardDisplay : MonoBehaviour
     [SerializeField] Transform charStand;
     [SerializeField] TextMesh cardName, atk, hp, ability1, ability2;
     [SerializeField] Image diceCost1, diceCost2, itemSlotCount, IconOfClass;
-   
+
+    Class character;
     private void Start()
     {
+        SetUpDefault();
+        Instantiate(character.gameObject, charStand);
+    }
+   
+    public void SetUpDefault()
+    {
         //set up sprite
-        var character = Char.GetComponent<Class>();
+        character = Char.GetComponent<Class>();
         Sprite[] costList = Resources.LoadAll<Sprite>("Sprites/Dice/DiceStyle1");
         Sprite[] itemSlotList = Resources.LoadAll<Sprite>("Sprites/BaseCard/ItemSlot");
-        Sprite charClass = Resources.Load<Sprite>("Sprites/Class/"+character.Classname);
+        Sprite charClass = Resources.Load<Sprite>("Sprites/Class/" + character.Classname);
         //set up stat
         cardName.text = character.Classname;
         atk.text = character.Dame.ToString();
         hp.text = character.Hp.ToString();
         ability1.text = character.AbilityList[0].Name;
         ability2.text = character.AbilityList[1].Name;
-       //set item slot
+        //set item slot
         if (character.Items.Length != 0)
         {
             itemSlotCount.sprite = itemSlotList[character.Items.Length];
@@ -31,11 +39,24 @@ public class CardDisplay : MonoBehaviour
         //set cost
         if (character.AbilityList.Length != 0)
         {
-            diceCost1.sprite = costList[character.AbilityList[0].DiceCost ];
-            diceCost2.sprite = costList[character.AbilityList[1].DiceCost ];
+            diceCost1.sprite = costList[character.AbilityList[0].DiceCost];
+            diceCost2.sprite = costList[character.AbilityList[1].DiceCost];
         }
         IconOfClass.sprite = charClass;
-        Instantiate(character.gameObject, charStand);
+    }
+    public void DisplayAbilityDesc(int number)
+    {
+        //may be can replace by list of ability but 2 is the skill slot limit so i do this
+        switch (number)
+        {
+            case 0:
+                ability1.text = character.AbilityList[0].Descript;       
+                break;
+            case 1:
+                ability2.text = character.AbilityList[1].Descript;
+                break;
+        }
+        
     }
 
 }
